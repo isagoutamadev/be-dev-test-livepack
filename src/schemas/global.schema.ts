5ed4791ee3853531e46ebefb1d282d4b15b35ca6
isa.goutama <isagoutama.dev@gmail.com>
2023-05-22 23:53:11 +0700

    schema id number

--- src/schemas/global.schema.ts
+++ src/schemas/global.schema.ts
@@ -3,3 +3,7 @@ import Joi from "joi";
 export const UUIDSchema = Joi.object({
     id: Joi.string().uuid().required(),
 });
+
+export const IDSchema = Joi.object({
+    id: Joi.number().min(1).required(),
+});
