77b68d23417b9914d69231f259b53c16a94375d4
isa.goutama <isagoutama.dev@gmail.com>
2023-05-22 23:52:32 +0700

    user database refactor uuid to increment id

--- database/migrations/20221020051328_m_users.ts
+++ database/migrations/20221020051328_m_users.ts
@@ -3,13 +3,12 @@ import { Knex } from "knex";
 export async function up(knex: Knex): Promise<void> {
     return knex.schema.createTable("m_users", function (table) {
         table.increments("id").primary().notNullable();
-        table.uuid("uuid").unique().notNullable();
         table.string("username").unique().notNullable();
         table.string("password").notNullable();
-        table.uuid("created_by").nullable();
-        table.uuid("updated_by").nullable();
+        table.integer("created_by").unsigned().nullable();
+        table.integer("updated_by").unsigned().nullable();
         table.timestamps();
-        table.uuid("deleted_by").nullable();
+        table.integer("deleted_by").unsigned().nullable();
         table.timestamp("deleted_at").nullable();
     });
 }
