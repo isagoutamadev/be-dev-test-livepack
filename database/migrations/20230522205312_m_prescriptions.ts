027ff802e043d118e13e45d8bcb75ecccf10f72a
isa.goutama <isagoutama.dev@gmail.com>
2023-05-23 11:56:11 +0700

    comment

--- database/migrations/20230522205312_m_prescriptions.ts
+++ database/migrations/20230522205312_m_prescriptions.ts
@@ -6,7 +6,7 @@ export async function up(knex: Knex): Promise<void> {
         table.string("patient_name").notNullable();
         table.string("pharmacy").notNullable();
         table.string("doctor").notNullable();
-        table.integer("total_price").defaultTo(0).unsigned().comment("if status confirmed");
+        table.integer("total_price").defaultTo(0).unsigned().comment("filled if status confirmed");
         table.enu("status", ["created", "confirmed", "canceled"]).defaultTo("created").notNullable();
         table.integer("created_by").unsigned().nullable();
         table.integer("updated_by").unsigned().nullable();
